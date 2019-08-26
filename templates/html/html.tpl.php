560468586a8c031bf9864a757d6ea85a19ad20c5
estesr <estesr@si.edu>
2019-08-26 17:05:49 -0400

    disabled omega js so that small devices get correct styles

--- templates/html/html.tpl.php
+++ templates/html/html.tpl.php
@@ -2,8 +2,9 @@
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
 <head<?php print $rdf->profile; ?>>
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="initial-scale=1.0, width=device-width" user-scaleable="yes" />
   <?php print $head; ?>
-  <title><?php print $head_title; ?></title>  
+  <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
